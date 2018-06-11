d305b7d89d47bf6d8e093e5c9be637f073e97ad6
Sachkov Yuri <sachkovura2011@gmail.com>
2018-06-11 21:00:21 +0300

    The last for today

--- EulerWm/result_function.h
+++ EulerWm/result_function.h
@@ -7,7 +7,7 @@ double *result_function(double **matrix, double *vector, size_t N, double dx) {
 	out_vector[N-1] += matrix[0][N-2] * vector[N-1] + matrix[1][N-1] * vector[N-1];
 	for(size_t j = 1; j < N-1; ++j){
 		for (size_t i = 0; i < 3; ++i) {
-			out_vector[j] += matrix[i][j] * vector[i+j-1];
+			out_vector[j] += matrix[i][j] * vector[i + j - 1];
 		}
 	}
 	out_vector[0] -= 1 / dx;
